727536352a992c3cf02d4005dd6b5b4fe364b4f0
Furtive Pygmy <rzvgib001@gmail.com>
2019-09-28 08:39:23 +0400

    PostImageCarousel anchor points set up

--- src/components/post-image-carousel/PostImageCarousel.js
+++ src/components/post-image-carousel/PostImageCarousel.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Image, ScrollView } from 'react-native';
 
 import layout from '../../constants/layout';
 
 const { height, width } = layout.window;
 
 const PostImageCarousel = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const numberOfImages = images.length;
+
+  const handleHorizontalScroll = nativeEvent => {
+    const scrollX = nativeEvent.contentOffset.x;
+
+    return setImageIndex(Math.round(scrollX / width));
+  };
+
   return (
-    <ScrollView horizontal pagingEnabled>
-      {images.map((image, index) => (
-        <View key={index} style={{ height: width / 1.5, width }}>
-          <Image
-            source={{ uri: image }}
+    <View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        onScroll={({ nativeEvent }) => handleHorizontalScroll(nativeEvent)}
+        ref={ref => (horizontalScrollView = ref)}
+      >
+        {images.map((image, index) => (
+          <View key={index} style={{ height: width / 1.5, width }}>
+            <Image
+              source={{ uri: image }}
+              style={{
+                flex: 1,
+                height: null,
+                width: null
+              }}
+            />
+          </View>
+        ))}
+      </ScrollView>
+      <View
+        style={{
+          flexDirection: 'row',
+          position: 'absolute',
+          bottom: -26,
+          alignSelf: 'center'
+        }}
+      >
+        {images.map((image, index) => (
+          <View
+            key={index}
             style={{
-              flex: 1,
-              height: null,
-              width: null
+              height: 6,
+              width: 6,
+              borderRadius: 3,
+              backgroundColor: imageIndex === index ? 'orangered' : '#777',
+              marginRight: 4
             }}
           />
-        </View>
-      ))}
-    </ScrollView>
+        ))}
+      </View>
+    </View>
   );
 };
 
